d0b2be7a6f237ef32f4fb4275e92f6dc6fc905c3
MoonFeather <moonbite233@gmail.com>
2024-07-24 16:00:04 +0800

    change the code structure

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,30 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "calculate.h"
+#include "./Form/ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    for(int i = 0; i < 16; i++) {
+        auto *button = this->findChild<QPushButton*>(QString("button_%1").arg(i));
+        if(button) {
+            connect(button, &QPushButton::clicked, this, &MainWindow::onButtonClicked);
+        }
+    }
+    connect(ui->lineEdit, &QLineEdit::textChanged, this, &MainWindow::changeResult);
 }
+
+void MainWindow::onButtonClicked() {
+    auto *button = qobject_cast<QPushButton*>(sender());
+    if(button->text() == u"=") return;
+    ui->lineEdit->insert(button->text());
+}
+void MainWindow::changeResult(const QString &text) const {
+    ui->label->setText(solve(text));
+}
+
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,12 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
+private slots:
+    void onButtonClicked();
+    void changeResult(const QString &text) const;
 private:
     Ui::MainWindow *ui;
 };
